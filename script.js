9d5207e77475564f0541c142eb23d7d55525a7d9
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-07-20 16:30:07 +0300

    proper construction of delta function

--- script.js
+++ script.js
@@ -5,94 +5,137 @@ var ctx = canvas.getContext("2d");
 ctx.fillStyle = 'black';
 ctx.strokeStyle = 'black';
 ctx.lineWidth = 3;
-const count=6;
+const count = 6;
 var alpha;
-const deltaInput=document.getElementById("delta");
+const deltaInput = document.getElementById("delta");
 deltaInput.setAttribute("style", "height:" + (deltaInput.scrollHeight) + "px;overflow-y:hidden;");
 deltaInput.addEventListener("input", OnInput, false);
 
+class Automaton {
+    constructor() {
+        this.states = undefined;
+        this.delta = undefined;
+        this.alpha = undefined;
+    }
+}
+
+var A = new Automaton();
+
 function OnInput() {
-  this.style.height = 0;
-  this.style.height = (this.scrollHeight) + "px";
+    this.style.height = 0;
+    this.style.height = (this.scrollHeight) + "px";
 }
 
-var states=undefined;
 
+document.getElementById("alphabet").oninput = getAlpha;
 
-document.getElementById("alphabet").oninput=getAlpha;
-
-function getAlpha(){
-    const txt=document.getElementById("alphabet").value;
-    var comma=(txt.slice(-1)==",");
-    alpha=txt.split(",");
+function getAlpha() {
+    const txt = document.getElementById("alphabet").value;
+    var comma = (txt.slice(-1) == ",");
+    alpha = txt.split(",");
     for (let i = 0; i < alpha.length; i++) {
-        alpha[i]=alpha[i].trim(); 
+        alpha[i] = alpha[i].trim();
     }
-    alpha=alpha.filter((str) => str != "");
-    console.log(alpha);
-    document.getElementById("alphabet").value=alpha;
+    alpha = alpha.filter((str) => str != "");
+    document.getElementById("alphabet").value = alpha;
     if (comma) {
-        document.getElementById("alphabet").value+=",";
+        document.getElementById("alphabet").value += ",";
+    }
+    A.alpha = new Map();
+    var letterCount = 0;
+    for (l of alpha) {
+        if (!A.alpha.has(l)) {
+            A.alpha.set(l, letterCount);
+            ++letterCount;
+        }
     }
 }
 
-function buildAutomata()
-{
-    var lines=deltaInput.value.split("\n");
-    console.log(lines);
-    states=new Set();
+function buildAutomata() {
+    getAlpha();
+    var lines = deltaInput.value.split("\n");
+    A.states = new Map();
+    var stateIndex = 0;
     for (let i = 0; i < lines.length; i++) {
-        lines[i]=lines[i].split("-");
-        if(lines[i].length!=3){alert("bad delta function...");states=undefined;return;}
+        lines[i] = lines[i].split("-");
+        if (lines[i].length != 3) { alert("bad delta function..."); A.states = undefined; return; }
         for (let j = 0; j < lines[i].length; j++) {
-            lines[i][j]=lines[i][j].trim();
-            if(j<2)states.add(lines[i][j]);
+            lines[i][j] = lines[i][j].trim();
+            if (j < 2) {
+                if (!A.states.has(lines[i][j])) {
+                    A.states.set(lines[i][j], stateIndex);
+                    ++stateIndex;
+                }
+            }
         }
     }
 
-    console.log(states);
+    A.delta = new Array(A.states.size);
+    for (let i = 0; i < A.delta.length; i++) {
+        A.delta[i] = new Array(A.alpha.size);
+    }
+
+    var ruleCount = 0;
+    for (const line of lines) {
+        console.log(line);
+        if (typeof A.delta[A.states.get(line[0])][A.alpha.get(line[2])] === 'undefined') ++ruleCount;
+        A.delta[A.states.get(line[0])][A.alpha.get(line[2])] = A.states.get(line[1]);
+        //todo remove console.log("set " + line[0] + "->" + line[2] + "to " + A.delta[A.states.get(line[0])][A.alpha.get(line[2])])
+    }
+    if (ruleCount != A.states.size * A.alpha.size) {
+        alert("bad delta function...\nautomaton is not a DFA...");
+        A = new Automaton();
+        return;
+    }
 
     drawScene();
 
-    //todo build delta function as table
     //todo finish -> drawScene
 }
 
-document.getElementById("build").onclick=buildAutomata;
+document.getElementById("build").onclick = buildAutomata;
 
-document.getElementById("check").onclick=getAlpha;
+document.getElementById("check").onclick = getAlpha;
 
-pArray=[];
+pArray = [];
 
 function distance2D(A, B) {
     return Math.sqrt((A[0] - B[0]) * (A[0] - B[0]) + (A[1] - B[1]) * (A[1] - B[1]));
 }
 
-function getRandomInt(from,to) {
-    return Math.floor(Math.random() * (to-from+1))+from;
+function getRandomInt(from, to) {
+    return Math.floor(Math.random() * (to - from + 1)) + from;
 }
 
-function generatePoints()
-{
-    pArray=[];
+function generatePoints() {
+    pArray = [];
     for (let index = 0; index < count; index++) {
-        const element = new Point(getRandomInt(-400,400),getRandomInt(-200,200));
+        const element = new Point(getRandomInt(-400, 400), getRandomInt(-200, 200));
         pArray.push(element);
     }
-    pArray.sort(function(a,b){return a.x-b.x});
+    pArray.sort(function (a, b) { return a.x - b.x });
 }
 
-function drawScene()
-{
-    if(typeof states === 'undefined'||states.size==0)
-    {alert("automata not built yet...\nsome error should be fixed...");return;}
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    var angle=2*Math.PI/states.size;
-    console.log(angle);
+function drawScene() {
+    if (typeof A.states === 'undefined' || A.states.length == 0) { alert("automata not built yet...\nsome error should be fixed..."); return; }
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var angle = 2 * Math.PI / A.states.size;
+    let i = 0;
+    for (const state of A.states) {
+        drawState(state[0], Math.cos(i * angle), Math.sin(i * angle));
+        ++i;
+    }
 }
 
-function drawState(name,pos)
-{
-
-    //todo
+function drawState(name, posX, posY) {
+    ctx.beginPath();
+    posX = w / 2 + posX * (Math.min(w, h) - 50) / 2;
+    posY = h / 2 - posY * (Math.min(w, h) - 50) / 2;
+    ctx.arc(posX, posY, 20, 0, 2 * Math.PI);
+    ctx.stroke();
+    ctx.textAlign = "center";
+    ctx.textBaseline = 'middle';
+    ctx.fillText(name, posX, posY);
 }
+
+drawState("state1", .5, .5);
