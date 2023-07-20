b57455271b7f54cf1e4a3f902b2548ffac6a82d0
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2023-07-20 12:40:34 +0300

    finding states

--- script.js
+++ script.js
@@ -7,6 +7,16 @@ ctx.strokeStyle = 'black';
 ctx.lineWidth = 3;
 const count=6;
 var alpha;
+const deltaInput=document.getElementById("delta");
+deltaInput.setAttribute("style", "height:" + (deltaInput.scrollHeight) + "px;overflow-y:hidden;");
+deltaInput.addEventListener("input", OnInput, false);
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+}
+
+var states=undefined;
 
 
 document.getElementById("alphabet").oninput=getAlpha;
@@ -27,7 +37,28 @@ function getAlpha(){
 }
 
 function buildAutomata()
-{}
+{
+    var lines=deltaInput.value.split("\n");
+    console.log(lines);
+    states=new Set();
+    for (let i = 0; i < lines.length; i++) {
+        lines[i]=lines[i].split("-");
+        if(lines[i].length!=3){alert("bad delta function...");states=undefined;return;}
+        for (let j = 0; j < lines[i].length; j++) {
+            lines[i][j]=lines[i][j].trim();
+            if(j<2)states.add(lines[i][j]);
+        }
+    }
+
+    console.log(states);
+
+    drawScene();
+
+    //todo build delta function as table
+    //todo finish -> drawScene
+}
+
+document.getElementById("build").onclick=buildAutomata;
 
 document.getElementById("check").onclick=getAlpha;
 
@@ -53,10 +84,15 @@ function generatePoints()
 
 function drawScene()
 {
-    ctx.clearRect(0,0,canvas.width,canvas.height)
-    pArray.forEach(element => {
-        element.draw();
-    });
+    if(typeof states === 'undefined'||states.size==0)
+    {alert("automata not built yet...\nsome error should be fixed...");return;}
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    var angle=2*Math.PI/states.size;
+    console.log(angle);
 }
 
-drawScene();
+function drawState(name,pos)
+{
+
+    //todo
+}
